1d742322ea6dd3fc50f5745ae7d84b8a14cb8ef7
lhy <dev@lhy.kr>
2023-04-27 19:44:52 +0900

    DataFrame처리함수 분리

--- app/questions/views.py
+++ app/questions/views.py
@@ -4,6 +4,11 @@ import pandas as pd
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from pandas import DataFrame
+
+
+def edit_df(df: DataFrame):
+    df.iloc[0, 0] = "Edit first cell"
 
 
 @csrf_exempt
@@ -11,6 +16,9 @@ def excel_to_pandas(request):
     if request.method == "POST":
         file = request.FILES.get("file")
         df = pd.read_excel(file)
+
+        # 다른 함수에서 DataFrame편집
+        edit_df(df)
 
         with BytesIO() as b:
             writer = pd.ExcelWriter(b, engine="openpyxl")
